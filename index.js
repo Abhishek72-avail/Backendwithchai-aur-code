19f3ae99b4c8ce43d01540398b0a16bbdb9eb5d0
abhishek <abhishekprasad7250@gmail.com>
2025-01-14 10:36:39 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const port = 3001
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+
+})
+
+  app.get('/apple', (req, res) => {
+    res.send('welcome to apple page')
+
+})
+
+    app.get('/main', (req, res) => {
+        res.send('wlecome to main page')
+ })
+
+    app.get('/youtube', (req,res) => {
+        res.send('<h2> welcome to youtube page </h2>')
+    })
+
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${port}`)
+})
